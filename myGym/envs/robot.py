a18629186547dcd02b1bfa18704527e10fc27a02
Megi Mejdrechova <megi.mejdrechova@cvut.cz>
2021-02-01 17:57:26 +0100

    fix physics settings, Vlado's recommendation

--- myGym/envs/robot.py
+++ myGym/envs/robot.py
@@ -104,6 +104,8 @@ class Robot:
                 pkg_resources.resource_filename("myGym",
                                                 self.robot_path),
                 self.position, self.orientation, useFixedBase=True, flags=(self.p.URDF_USE_SELF_COLLISION))
+        for jid in range(self.p.getNumJoints(self.robot_uid)):
+                self.p.changeDynamics(self.robot_uid, jid,  collisionMargin=0., contactProcessingThreshold=0.0, ccdSweptSphereRadius=0)
         # if 'jaco' in self.name: #@TODO jaco gripper has closed loop between finger and finger_tip that is not respected by the simulator
         #     self.p.createConstraint(self.robot_uid, 11, self.robot_uid, 15, self.p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
         #     self.p.createConstraint(self.robot_uid, 13, self.robot_uid, 17, self.p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0])
