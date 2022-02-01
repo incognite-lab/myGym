0cb21012fb14547fba300ad5c8b1194a1cb66dc0
michalvavrecka <michal.vavrecka@cvut.cz>
2022-02-01 15:12:28 +0100

    new configs

--- myGym/multitester-speed.py
+++ myGym/multitester-speed.py
@@ -9,23 +9,20 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-cfg", "--config", type=str, default="./configs/speed_reach.json", help="config file for evaluation")
+parser.add_argument("-cfg", "--config", type=str, default="./configs/train_pnp_new.json", help="config file for evaluation")
 parser.add_argument("-rob", "--robot",  default=["kuka_magnetic"], nargs='*', help="what robots to test")
 parser.add_argument("-ra", "--robotaction",  default=["joints","step","absolute"], nargs='*', help="what actions to test")
 parser.add_argument("-ar", "--action_repeat", default=[1,5,10], nargs='*', help="simuilation steps without env action")
-parser.add_argument("-mv", "--max_velocity", default=[.1,1,10], nargs='*', help="arm speed")
-parser.add_argument("-mf", "--max_force", default=[1,30,100], nargs='*', help="arm speed")
+parser.add_argument("-mv", "--max_velocity", default=[0.1,0.5,1,10], nargs='*', help="arm speed")
+parser.add_argument("-mf", "--max_force", default=[1,30,100,300], nargs='*', help="arm speed")
 parser.add_argument("-thread", "--threaded", type=bool, default="True", help="run in threads")
 parser.add_argument("-out", "--output", type=str, default="./trained_models/tester.json", help="output file")
 
 args = parser.parse_args()
 
 parameters = {
-    "robot": args.robot,
-    "robot_action": args.robotaction,
     "max_velocity": args.max_velocity,
     "max_force": args.max_force,
-    "action_repeat": args.action_repeat,
 }
 parameter_grid = ParameterGrid(parameters)
 configfile = args.config
