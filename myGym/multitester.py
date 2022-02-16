f21ebfb094b7e364f53292dd5cf95e007e84b521
michalvavrecka <michal.vavrecka@cvut.cz>
2022-02-16 10:33:28 +0100

    multitester update - cleaned duplicate version

--- myGym/multitester.py
+++ myGym/multitester.py
@@ -9,21 +9,24 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-cfg", "--config", type=str, default="./configs/tester.json", help="config file for evaluation")
-parser.add_argument("-rob", "--robot",  default=["kuka"], nargs='*', help="what robots to test")
-parser.add_argument("-ra", "--robotaction",  default=["step"], nargs='*', help="what actions to test")
+parser.add_argument("-cfg", "--config", type=str, default="./configs/train_pnp_new.json", help="config file for evaluation")
+parser.add_argument("-b", "--robot",  default=["kuka_magnetic","[panda_bullet"], nargs='*', help="what robots to test")
+parser.add_argument("-ba", "--robot_action",  default=["joints","absolute","step"], nargs='*', help="what actions to test")
+parser.add_argument("-ar", "--action_repeat", default=[1,5,10], nargs='*', help="simuilation steps without env action")
+parser.add_argument("-mv", "--max_velocity", default=[1,5,10,20], nargs='*', help="arm speed")
+parser.add_argument("-mf", "--max_force", default=[1,50,100,300], nargs='*', help="arm force")
 parser.add_argument("-frame", "--framework", default=["tensorflow"], nargs='*', help="what algos to test")
-parser.add_argument("-algo", "--algorithms", default=["ppo", "ppo2", "sac", "acktr", "ddpg", "td3", "trpo", "a2c", "myalgo"], nargs='*', help="what algos to test")
+#parser.add_argument("-algo", "--algorithms", default=["ppo", "ppo2", "sac", "acktr", "ddpg", "td3", "trpo", "a2c", "myalgo","multi"], nargs='*', help="what algos to test")
+parser.add_argument("-algo", "--algorithms", default=["ppo", "ppo2","multi"], nargs='*', help="what algos to test")
 parser.add_argument("-thread", "--threaded", type=bool, default="True", help="run in threads")
-parser.add_argument("-out", "--output", type=str, default="./trained_models/tester.json", help="output file")
+parser.add_argument("-out", "--output", type=str, default="./trained_models/testernew.json", help="output file")
 
 args = parser.parse_args()
 
 parameters = {
     "robot": args.robot,
-    "robot_action": args.robotaction,
+    "robot_action": args.robot_action,
     "algo": args.algorithms,
-    "train_framework": args.framework,
 }
 parameter_grid = ParameterGrid(parameters)
 configfile = args.config
