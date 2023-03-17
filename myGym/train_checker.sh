9099dc28ece95675739edc52f42c549fdda3b793
Michal Vavrecka <michal.vavrecka@cvut.cz>
2023-03-17 16:05:13 +0100

    small prerease bugfix

--- myGym/train_checker.sh
+++ myGym/train_checker.sh
@@ -2,6 +2,6 @@
 #This is minimal unit test to check, whether the current version of myGym is able to train. 
 # It will do simle train and evaluate in 2 minutes. If success rate is above 30 percdents, it works.
 rm -r ./trained_models/check
-python train.py --config ./configs/train_reach.json --algo ppo2 --max_episode_steps 256 --algo_steps 256 --steps 20000 --eval_episodes 10 --logdir ./trained_models/check
-python test.py -cfg ./trained_models/check/reach_table_kuka_joints_ppo2/train.json
-rm -r ./trained_models/check
+python train.py --config ./configs/train_reach.json --gui 1 --algo ppo2 -b kuka -ba joints --max_episode_steps 256 --algo_steps 256 --steps 20000 --eval_episodes 10 --logdir ./trained_models/check
+#python test.py -cfg ./trained_models/check/reach_table_kuka_joints_ppo2/train.json
+#rm -r ./trained_models/check
