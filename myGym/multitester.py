71d691e939b296d1592defe287f1a8d32c1683ab
michalvavrecka <michal.vavrecka@cvut.cz>
2023-08-04 17:45:24 +0200

    multitester update - automatically detect multiple parameter values from command line and creates threaded combinations

--- myGym/multitester.py
+++ myGym/multitester.py
@@ -10,28 +10,41 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-cfg", "--config", type=str, default="./configs/debugdist.json", help="config file for evaluation")
-parser.add_argument("-b", "--robot",  default=["kuka",'panda','panda1'], nargs='*', help="what robots to test")
+#robot
+parser.add_argument("-b", "--robot",  default=["kuka"], nargs='*', help="what robots to test")
 parser.add_argument("-ba", "--robot_action",  default=["joints"], nargs='*', help="what actions to test")
 parser.add_argument("-ar", "--action_repeat", default=[1], nargs='*', help="simuilation steps without env action")
 parser.add_argument("-mv", "--max_velocity", default=[3], nargs='*', help="arm speed")
 parser.add_argument("-mf", "--max_force", default=[100], nargs='*', help="arm force")
+#task
+parser.add_argument("-tt", "--task_type", default=["reach"], nargs='*',  help="Type of task to learn: reach, push, throw, pick_and_place")
+
 parser.add_argument("-frame", "--framework", default=["tensorflow"], nargs='*', help="what algos to test")
-parser.add_argument("-algo", "--algorithms", default=["ppo2", "acktr","multi"], nargs='*', help="what algos to test")
+parser.add_argument("-algo", "--algorithms", default=["acktr"], nargs='*', help="what algos to test")
 #parser.add_argument("-algo", "--algorithms", default=["ppo2","acktr","multi"], nargs='*', help="what algos to test")
 parser.add_argument("-l", "--logdir",type=str, default="./trained_models/pnr", help="where to save the results")
 parser.add_argument("-thread", "--threaded", type=bool, default="True", help="run in threads")
-parser.add_argument("-out", "--output", type=str, default="./trained_models/pnr.json", help="output file")
+parser.add_argument("-out", "--output", type=str, default="./trained_models/multitester.json", help="output file")
 
 args = parser.parse_args()
 
-parameters = {
-    "robot": args.robot,
-    "robot_action": args.robot_action,
-    "algo": args.algorithms,
-    "max_velocity": args.max_velocity,
-    "max_force": args.max_force,
-    "action_repeat": args.action_repeat
-}
+parameters = {}
+# find args with multiple values
+for arg in vars(args):
+    if type(getattr(args, arg)) == list:
+        if len(getattr(args, arg)) > 1:
+            #add arg as key to parameters dict and gettatr(args, arg) as value
+            parameters[arg] = getattr(args, arg)   
+
+
+#parameters = {
+#    "robot": args.robot,
+#    "robot_action": args.robot_action,
+#    "algo": args.algorithms,
+#    "max_velocity": args.max_velocity,
+#    "max_force": args.max_force,
+#    "action_repeat": args.action_repeat
+#}
 parameter_grid = ParameterGrid(parameters)
 configfile = args.config
 logdirfile = args.logdir
