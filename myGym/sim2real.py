e5cedd88d89764818706725b11e2b8cb10a5b09c
Michal Vavrecka <michal.vavrecka@cvut.cz>
2024-01-22 18:20:21 +0100

    script update + nicomotion classes and configs

--- myGym/sim2real.py
+++ myGym/sim2real.py
@@ -13,7 +13,57 @@ import pkg_resources
 import random
 import getkey
 import signal
-from utils.nicomotors import NicoMotors
+from nicomotion.Motion import Motion
+from utils.nicocameras import NicoCameras, image_shift_xy
+from utils.nicodummy import DummyRobot
+import serial
+
+def quit():
+    os._exit(0)
+
+# exit on ctrl-c
+def signal_handler(signal, frame):
+    os._exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+motorConfig = './nico_humanoid_upper_rh7d_ukba.json'
+try:
+    robot = Motion(motorConfig=motorConfig)
+except:
+    robot = DummyRobot()
+    print('motors are not operational')
+
+defaultSpeed = 0.04*0.5
+
+safe = { # standard position
+                'l_shoulder_z':0.0,
+                'l_shoulder_y':0.0,
+                'l_arm_x':0.0,
+                'l_elbow_y':89.0,
+                'l_wrist_z':0.0,
+                'l_wrist_x':-56.0,
+                'l_thumb_z':-57.0,
+                'l_thumb_x':-180.0,
+                'l_indexfinger_x':-180.0,
+                'l_middlefingers_x':-180.0,
+                'r_shoulder_z':0.0,
+                'r_shoulder_y':0.0,
+                'r_arm_x':0.0,
+                'r_elbow_y':89.0,
+                'r_wrist_z':0.0,
+                'r_wrist_x':-56.0,
+                'r_thumb_z':-57.0,
+                'r_thumb_x':-180.0,
+                'r_indexfinger_x':-180.0,
+                'r_middlefingers_x':-180.0,
+                'head_z':0.0,
+                'head_y':0.0
+            }
+for k in safe.keys():
+    robot.setAngle(k,safe[k],defaultSpeed)
+
+
 
 
 clear = lambda: os.system('clear')
@@ -153,16 +203,6 @@ def detect_key(keypress,arg_dict,action):
 
 def test_env(env, arg_dict):
     real_robot = True
-    motors = NicoMotors()
-    dofs = motors.dofs()
-    try:
-        motors.open()
-    except:
-        print('motors are not operational')
-    
-    for k in dofs:
-        motors.enableTorque(k)
-        torque = True
     
     
     arg_dict["vsampling"] = 0
@@ -304,7 +344,9 @@ def test_env(env, arg_dict):
 
             print (f"Action:{action}")
             observation, reward, done, info = env.step(action)
+            
             if real_robot:
+                input("Press Enter to continue...")
                 for k in dofs:
                     motors.setPositionDg(k,int(values[k]))
 
