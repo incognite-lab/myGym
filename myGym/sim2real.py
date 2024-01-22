79dca09d50d05d3ff0fe532ad5f510ab81c054a9
Michal Vavrecka <michal.vavrecka@cvut.cz>
2024-01-22 19:52:08 +0100

    sim to real tests - two joints working

--- myGym/sim2real.py
+++ myGym/sim2real.py
@@ -27,41 +27,27 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-motorConfig = './nico_humanoid_upper_rh7d_ukba.json'
-try:
-    robot = Motion(motorConfig=motorConfig)
-except:
-    robot = DummyRobot()
-    print('motors are not operational')
+def init_robot():
+    motorConfig = './nico_humanoid_upper_rh7d_ukba.json'
+    try:
+        robot = Motion(motorConfig=motorConfig)
+    except:
+        robot = DummyRobot()
+        print('motors are not operational')
 
-defaultSpeed = 0.04*0.5
 
-safe = { # standard position
-                'l_shoulder_z':0.0,
-                'l_shoulder_y':0.0,
-                'l_arm_x':0.0,
-                'l_elbow_y':89.0,
-                'l_wrist_z':0.0,
-                'l_wrist_x':-56.0,
-                'l_thumb_z':-57.0,
-                'l_thumb_x':-180.0,
-                'l_indexfinger_x':-180.0,
-                'l_middlefingers_x':-180.0,
+    defaultSpeed = 0.04*0.5
+
+    safe = { # standard position
                 'r_shoulder_z':0.0,
-                'r_shoulder_y':0.0,
+                'r_shoulder_y':90.0,
                 'r_arm_x':0.0,
-                'r_elbow_y':89.0,
+                'r_elbow_y':120.0,
                 'r_wrist_z':0.0,
-                'r_wrist_x':-56.0,
-                'r_thumb_z':-57.0,
-                'r_thumb_x':-180.0,
-                'r_indexfinger_x':-180.0,
-                'r_middlefingers_x':-180.0,
-                'head_z':0.0,
-                'head_y':0.0
+                'r_wrist_x':-56.0
             }
-for k in safe.keys():
-    robot.setAngle(k,safe[k],defaultSpeed)
+    for k in safe.keys():
+        robot.setAngle(k,safe[k],defaultSpeed)
 
 
 
@@ -203,7 +189,28 @@ def detect_key(keypress,arg_dict,action):
 
 def test_env(env, arg_dict):
     real_robot = True
-    
+    if real_robot:
+         #init_robot()
+        motorConfig = './nico_humanoid_upper_rh7d_ukba.json'
+        try:
+            robot = Motion(motorConfig=motorConfig)
+        except:
+            robot = DummyRobot()
+            print('motors are not operational')
+
+
+        defaultSpeed = 0.04*0.5
+
+        safe = { # standard position
+                'r_shoulder_z':0.0,
+                'r_shoulder_y':90.0,
+                'r_arm_x':0.0,
+                'r_elbow_y':120.0,
+                'r_wrist_z':0.0,
+                'r_wrist_x':-56.0
+            }
+        for k in safe.keys():
+            robot.setAngle(k,safe[k],defaultSpeed)
     
     arg_dict["vsampling"] = 0
     arg_dict["vinfo"] = 0
@@ -341,14 +348,24 @@ def test_env(env, arg_dict):
                 action = env.action_space.sample()
 
             
-
-            print (f"Action:{action}")
+            
+            deg = np.rad2deg(action)
+            print (f"Action:{deg}")
             observation, reward, done, info = env.step(action)
             
             if real_robot:
-                input("Press Enter to continue...")
-                for k in dofs:
-                    motors.setPositionDg(k,int(values[k]))
+                defaultSpeed = 0.08
+
+                safe = { # standard position
+                'r_shoulder_z':0.0,
+                'r_shoulder_y':90.0,
+                'r_arm_x':0.0,
+                'r_elbow_y':120.0,
+                'r_wrist_z':0.0,
+                'r_wrist_x':-56.0
+            }
+                robot.setAngle('r_shoulder_z',deg[0],defaultSpeed)
+                robot.setAngle('r_shoulder_y',deg[1],defaultSpeed)
 
             if arg_dict["vtrajectory"] == True:
                 visualize_trajectories(info,action)
