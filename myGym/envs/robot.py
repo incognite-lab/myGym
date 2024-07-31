9093f1eba52bfefa04849b3be2a5964a6fcadbce
Michal Vavrecka <michal.vavrecka@cvut.cz>
2024-07-31 16:57:21 +0200

    verbosity bugfix - robot and table urdf missing inertia fixed, robot summary converted to self.debug mode

--- myGym/envs/robot.py
+++ myGym/envs/robot.py
@@ -42,6 +42,7 @@ class Robot:
                  max_force = None, #50.,
                  pybullet_client=None):
 
+        self.debug = False
         self.p = pybullet_client
         self.robot_dict = get_robot_dict()
         self.robot_path = self.robot_dict[robot]['path']
@@ -128,16 +129,18 @@ class Robot:
                 self.gripper_indices.append(i)
                 self.gjoint_positions.append(self.p.getJointState(self.robot_uid,i)[0])
 
-        print("Robot summary")
-        print("--------------")
-        print("Links:")
-        print("\n".join(map(str,self.link_names)))
-        print("Joints:")
-        print("\n".join(map(str,self.motor_names)))
-        print("Gripper joints:")
-        print("\n".join(map(str,self.gripper_names)))
-        print("Gripper index is: " + str(self.gripper_index))
-        print("End effector index is: " + str(self.end_effector_index))
+        if self.debug:
+            print("Robot summary")
+            print("--------------")
+            print("Links:")
+            print("\n".join(map(str,self.link_names)))
+            print("Joints:")
+            print("\n".join(map(str,self.motor_names)))
+            print("Gripper joints:")
+            print("\n".join(map(str,self.gripper_names)))
+            print("Gripper index is: " + str(self.gripper_index))
+            print("End effector index is: " + str(self.end_effector_index))
+        
         self.joints_num = len(self.motor_names)
         self.gjoints_num = len(self.gripper_names)
 
