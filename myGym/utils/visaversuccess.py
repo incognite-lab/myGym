0c7dcca645a98739cba83a7dcdb326a6e1e02e8f
cybersuslik <sof4609@gmail.com>
2024-09-13 13:25:26 +0200

    visaversuccess: fixed mean values calculations

--- myGym/utils/visaversuccess.py
+++ myGym/utils/visaversuccess.py
@@ -1,13 +1,14 @@
 import argparse
+import colorsys
 import json
 import os
-import random
 import re
+from math import ceil as ceiling
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import yaml
-import colorsys
 
 # Color mapping for algorithms
 color_map = {
@@ -47,10 +48,12 @@ def hex_to_rgb(hex_color):
     r, g, b = int(hex_color[0:2], 16), int(hex_color[2:4], 16), int(hex_color[4:6], 16)
     return r / 255.0, g / 255.0, b / 255.0
 
+
 def rgb_to_hex(r, g, b):
     """Convert an RGB tuple to a hex color string."""
     r, g, b = int(r * 255), int(g * 255), int(b * 255)
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 
 def brighten_color(hex_color, brightness_increase=0.0, saturation_increase=0.15):
     """Brighten and increase the intensity of a color."""
@@ -73,6 +76,7 @@ def atoi(text):
 def natural_keys(text):
     """Split text into natural sort keys."""
     return [atoi(c) for c in re.split(r'(\d+)', text)]
+
 
 def cfg_string2dict(cfg_raw):
     """Convert a configuration string to a dictionary."""
@@ -113,12 +117,13 @@ def multi_dict_diff_by_key(dict_list):
     """Compare dictionaries by keys and return differences and similarities."""
     diff_dict, all_same = multi_dict_diff_scary(dict_list)
     return {k: v for i, (k, v) in enumerate(diff_dict.items()) if not all_same[i]}, \
-           {k: v for i, (k, v) in enumerate(diff_dict.items()) if all_same[i]}
+        {k: v for i, (k, v) in enumerate(diff_dict.items()) if all_same[i]}
 
 
 def get_arguments():
     """Parse and return command-line arguments."""
     parser = argparse.ArgumentParser()
+    # '/home/student/mygym/myGym/weight_vis2/SB3_100mil/AGMDW_SB3'
     parser.add_argument("--pth", default='/home/student/mygym/myGym/weight_visualizer/AGMDW_stable')
     parser.add_argument("--robot", default=["kuka", "panda"], nargs='*')
     parser.add_argument("--algo", default=["multiacktr", "multippo2", "ppo2", "ppo", "acktr", "sac", "ddpg",
@@ -145,6 +150,12 @@ def ax_plot(ax, steps, data, color, label):
     """Plot data on the given axis."""
     ax.plot(steps, data, color=color, linestyle='solid', linewidth=3, marker='o', markerfacecolor=color,
             markersize=4, label=label)
+
+
+def ax_fill(ax, steps, meanvalue, data, index, color):
+    """Plot data on the given axis."""
+    ax.fill_between(steps, meanvalue - np.std(np.take(data, index, 0), 0),
+                    meanvalue + np.std(np.take(data, index, 0), 0), color=color, alpha=0.2)
 
 
 def main():
@@ -200,7 +211,6 @@ def main():
             for key, val in dict_acts.items():
                 if key in task:
                     acts.append(val)
-            print(acts)
 
             success.append(x[3])
             mean_distance_error.append(x[4])
@@ -231,63 +241,85 @@ def main():
         mean_subgoals_steps[i] = [[mean_subgoals_steps[i][k][j] for k in range(len(mean_subgoals_steps[i]))]
                                   for j in range(len(mean_subgoals_steps[i][0]))]
 
-
     # Get differences between configs
     diff, same = multi_dict_diff_by_key(configs)
+    plot_num = len(set(diff['algo'])) if 'algo' in diff.keys() else 1
+    if plot_num == 2:
+        plot_num = 3
+    ticks = list(range(0, 101, int(ticks_num)))
+    width = steps[1] - steps[0]
+
     # Plotting
     fig1 = plt.figure(1, figsize=(35, 30))
     ax1, ax2, ax3, ax4, ax5 = [fig1.add_subplot(3, 2, i) for i in range(1, 6)]
 
-    fig2 = plt.figure(2, figsize=(35, 30))
+    fig2 = plt.figure(2, figsize=(13 * ceiling(plot_num / 2), 10 * ceiling(plot_num / 2)))
 
-    ticks = list(range(0, 101, int(ticks_num)))
     counter = 0
-
     for d in range(len(success)):
         success[d] = np.delete(success[d], np.s_[min_steps:])
+        mean_reward[d] = np.delete(mean_reward[d], np.s_[min_steps:])
+        std_reward[d] = np.delete(std_reward[d], np.s_[min_steps:])
+        mean_steps[d] = np.delete(mean_steps[d], np.s_[min_steps:])
+        mean_distance_error[d] = np.delete(mean_distance_error[d], np.s_[min_steps:])
+        mean_subgoals_finished[d] = np.delete(mean_subgoals_finished[d], np.s_[min_steps:])
+        for i, elem in enumerate(mean_subgoals_steps[d]):
+            mean_subgoals_steps[d][i] = np.delete(elem, np.s_[min_steps:])
     if 'algo' in diff.keys() and len(args.algo) > 1:
         index = [[] for _ in range(len(args.algo))]
         for i, algo in enumerate(args.algo):
             for j, diffalgo in enumerate(diff['algo']):
                 if algo == diffalgo:
                     index[i].append(j)
-
             if index[i]:
                 meanvalue = np.mean(np.take(success, index[i], 0), 0)
                 ax_plot(ax1, steps, meanvalue, color_map[algo], algo)
-                ax1.fill_between(steps,
-                                 np.mean(np.take(success, index[i], 0), 0) - np.std(np.take(success, index[i], 0), 0),
-                                 np.mean(np.take(success, index[i], 0), 0) + np.std(np.take(success, index[i], 0), 0),
-                                 color=color_map[algo], alpha=0.2)
-                ax_plot(ax2, steps, mean_reward[counter], color_map[algo], algo)
-                ax2.plot(steps, std_reward[counter], alpha=0.8, color=color_map[algo],
+                ax_fill(ax1, steps, meanvalue, success, index[i], color_map[algo])
+
+                meanvalue_rew = np.mean(np.take(mean_reward, index[i], 0), 0)
+                ax_plot(ax2, steps, meanvalue_rew, color_map[algo], algo)
+                ax_fill(ax2, steps, meanvalue_rew, mean_reward, index[i], color_map[algo])
+
+                meanvalue_std = np.mean(np.take(std_reward, index[i], 0), 0)
+                ax2.plot(steps, meanvalue_std, alpha=0.8, color=color_map[algo],
                          linestyle='dotted', linewidth=3, marker='o', markerfacecolor=color_map[algo], markersize=4,
                          label=f"{algo} std")
-                ax_plot(ax3, steps, mean_steps[counter], color_map[algo], algo)
-                ax_plot(ax4, steps, mean_distance_error[counter], color_map[algo], algo)
+                ax_fill(ax2, steps, meanvalue_std, std_reward, index[i], color_map[algo])
+
+                meanvalue_steps = np.mean(np.take(mean_steps, index[i], 0), 0)
+                ax_plot(ax3, steps, meanvalue_steps, color_map[algo], algo)
+                ax_fill(ax3, steps, meanvalue_steps, mean_steps, index[i], color_map[algo])
+
+                meanvalue_error = np.mean(np.take(mean_distance_error, index[i], 0), 0)
+                ax_plot(ax4, steps, meanvalue_error, color_map[algo], algo)
+                ax_fill(ax4, steps, meanvalue_error, mean_distance_error, index[i], color_map[algo])
+
+                meanvalue_subgoals = np.mean(np.take(mean_subgoals_finished, index[i], 0), 0)
                 ax5.set_ylim([0, 100])
-                ax_plot(ax5, steps, mean_subgoals_finished[counter], color_map[algo], algo)
+                ax_plot(ax5, steps, meanvalue_subgoals, color_map[algo], algo)
+                ax_fill(ax5, steps, meanvalue_subgoals, mean_subgoals_finished, index[i], color_map[algo])
                 ax5.set_yticks(ticks)
 
-                ax = fig2.add_subplot(3, 2, counter + 1)
+                ax = fig2.add_subplot(ceiling(plot_num / 2), ceiling(plot_num / 2), counter + 1)
                 ax.set_ylim(0, 100)
                 label_counter = 1
                 bottom = [0] * len(steps)
-
+                meanvalue_subgoals_steps = np.mean(np.take(mean_subgoals_steps, index[i], 0), 0)
                 for l, _ in enumerate(mean_subgoals_steps[counter]):
                     if len(mean_subgoals_steps[counter][l]) != len(steps):
                         print(f"Data length mismatch: {len(mean_subgoals_steps[counter][l])} vs {len(steps)}")
                         continue
-                    p = ax.bar(x=steps, height=mean_subgoals_steps[counter][l], color=color_map_acts.get(acts[label_counter-1], "black"),
-                               label=f"{acts[label_counter-1]}", bottom=bottom, width=-500000, align='edge',
-                               edgecolor='white')
-                    bottom = [sum(x) for x in zip(bottom, mean_subgoals_steps[counter][l])]
+                    p = ax.bar(x=steps, height=meanvalue_subgoals_steps[l],
+                               color=color_map_acts.get(acts[label_counter - 1], "black"),
+                               label=f"{acts[label_counter - 1]}", bottom=bottom, width=-width, align='edge',
+                               edgecolor='black')
+                    bottom = [sum(x) for x in zip(bottom, meanvalue_subgoals_steps[l])]
 
                     ax.bar_label(
                         p,
-                        labels=[f"{v:.1f}" for v in mean_subgoals_steps[counter][l]],
+                        labels=[f"{v:.1f}" for v in meanvalue_subgoals_steps[l]],
                         label_type='center',
-                        color='white',
+                        color='black',
                         fontsize=13,
                         fmt='%g'
                     )
@@ -295,8 +327,6 @@ def main():
                 counter += 1
 
                 ax_set(ax, f'Mean subgoals steps over training steps, {algo}', 'Mean subgoals steps, %')
-
-
     elif 'robot' in diff.keys() and len(args.robot) > 1:
         index = [[] for _ in range(len(args.robot))]
         for i, robot in enumerate(args.robot):
@@ -313,10 +343,7 @@ def main():
                 plt.title("Robots")
     else:
         print("No data to compare")
-        meanvalue = np.mean(success, 0)
-        variance = np.std(success, 0)
-        ax_plot(ax1, steps, meanvalue, color_map[alg], alg)
-        ax1.fill_between(steps, meanvalue - variance, meanvalue + variance, color=color_map[alg], alpha=0.2)
+        ax_plot(ax1, steps, [item for row in success for item in row], color_map[alg], alg)
         ax_plot(ax2, steps, [item for row in mean_reward for item in row], color_map[alg], alg)
         ax2.plot(steps, [item for row in std_reward for item in row], alpha=0.8, color=color_map[alg],
                  linestyle='dotted', linewidth=3, marker='o', markerfacecolor=color_map[alg], markersize=4,
@@ -327,7 +354,7 @@ def main():
         ax_plot(ax5, steps, [item for row in mean_subgoals_finished for item in row], color_map[alg], alg)
         ax5.set_yticks(ticks)
 
-        ax = fig2.add_subplot(3, 2, counter + 1)
+        ax = fig2.add_subplot(ceiling(plot_num / 2), ceiling(plot_num / 2), counter + 1)
         ax.set_ylim(0, 100)
         label_counter = 1
         bottom = [0] * len(steps)
@@ -338,15 +365,15 @@ def main():
                 continue
             p = ax.bar(x=steps, height=mean_subgoals_steps[counter][l],
                        color=color_map_acts.get(acts[label_counter - 1], "black"),
-                       label=f"{acts[label_counter - 1]}", bottom=bottom, width=-500000, align='edge',
-                       edgecolor='white')
+                       label=f"{acts[label_counter - 1]}", bottom=bottom, width=-width, align='edge',
+                       edgecolor='black')
             bottom = [sum(x) for x in zip(bottom, mean_subgoals_steps[counter][l])]
 
             ax.bar_label(
                 p,
                 labels=[f"{v:.1f}" for v in mean_subgoals_steps[counter][l]],
                 label_type='center',
-                color='white',
+                color='black',
                 fontsize=13,
                 fmt='%g'
             )
