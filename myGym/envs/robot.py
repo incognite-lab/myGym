ad335c02186908441391b66f3d2f9d6441a0f1c4
Michal Vavrecka <michal.vavrecka@cvut.cz>
2024-11-18 13:21:18 +0100

    root cleaning, conda env update (gymnasium compatibility only 0.29.1), robot and table urdf update

--- myGym/envs/robot.py
+++ myGym/envs/robot.py
@@ -45,8 +45,9 @@ class Robot(EnvObject):
                  observation="ground_truth",
                  vae_path=None,
                  yolact_path=None,
-                 yolact_config=None):
-
+                 yolact_config=None,
+                 pybullet_client=None):
+        self.debug = False
         self.p = pybullet_client
         self.robot_dict = get_robot_dict()
         self.robot_path = self.robot_dict[robot]['path']
@@ -89,6 +90,7 @@ class Robot(EnvObject):
         #else:
         #self.init_joint_poses = np.zeros((len(self.motor_names)))
         #self.reset()
+
 
     def _load_robot(self):
         """
@@ -136,16 +138,17 @@ class Robot(EnvObject):
                 self.gripper_indices.append(i)
                 self.gjoint_positions.append(self.p.getJointState(self.robot_uid,i)[0])
 
-        print("Robot summary")
-        print("--------------")
-        print("Links:")
-        print("\n".join(map(str,self.link_names)))
-        print("Joints:")
-        print("\n".join(map(str,self.motor_names)))
-        print("Gripper joints:")
-        print("\n".join(map(str,self.gripper_names)))
-        print("Gripper index is: " + str(self.gripper_index))
-        print("End effector index is: " + str(self.end_effector_index))
+        if self.debug:
+            print("Robot summary")
+            print("--------------")
+            print("Links:")
+            print("\n".join(map(str,self.link_names)))
+            print("Joints:")
+            print("\n".join(map(str,self.motor_names)))
+            print("Gripper joints:")
+            print("\n".join(map(str,self.gripper_names)))
+            print("Gripper index is: " + str(self.gripper_index))
+            print("End effector index is: " + str(self.end_effector_index))
         self.joints_num = len(self.motor_names)
         self.gjoints_num = len(self.gripper_names)
 
