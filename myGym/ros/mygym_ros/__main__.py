5b9295fb52263d4caaf57f699b94a87fbfd4d210
radoskov <radoslav.skoviera@cvut.cz>
2025-07-18 10:27:40 +0200

    Fixed sys env to run the web app

--- myGym/ros/mygym_ros/__main__.py
+++ myGym/ros/mygym_ros/__main__.py
@@ -4,4 +4,5 @@ import os
 MAIN_MODULE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "recording_viz.py")
 
 if __name__ == "__main__":
-    call(["streamlit", "run", str(MAIN_MODULE_FILE)])
+    env = os.environ.copy()
+    call(["python", "-m", "streamlit", "run", str(MAIN_MODULE_FILE)], env=env)
