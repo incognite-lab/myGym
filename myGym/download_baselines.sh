a12ff54206d2553660bcd3023aa8df65d14e6e13
Michal Vavrecka <michal.vavrecka@cvut.cz>
2025-09-02 15:32:14 +0200

    new pretrained models download

--- myGym/download_baselines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -r --no-parent --reject "index.html*" https://data.ciirc.cvut.cz/public/groups/incognite/myGym/baselines/
-
-mkdir -p trained_models
-
-mv -T data.ciirc.cvut.cz/public/groups/incognite/myGym/baselines ./trained_models/baselines
-
-rm -r data.ciirc.cvut.cz
