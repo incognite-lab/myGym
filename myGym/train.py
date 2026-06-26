e3372e69cb833eb51f9e4ef67c0acdcee857ea59
Michal Vavrecka <michal.vavrecka@cvut.cz>
2026-06-26 09:32:37 +0200

    small bugfix in default config

--- myGym/train.py
+++ myGym/train.py
@@ -241,7 +241,7 @@ def train(env, implemented_combos, model_logdir, arg_dict, pretrained_model=None
 def get_parser():
     parser = argparse.ArgumentParser()
     # Environment
-    parser.add_argument("-cfg", "--config", type=str, default = "./configs/temp.json", help="Config file path") #./trained_models/AG/AG_table_tiago_tiago_dual_joints_gripper_multippo/train.json
+    parser.add_argument("-cfg", "--config", type=str, default = "./configs/AGM.json", help="Config file path") #./trained_models/AG/AG_table_tiago_tiago_dual_joints_gripper_multippo/train.json
     parser.add_argument("-n", "--env_name", type=str, help="Environment name")
     parser.add_argument("-ws", "--workspace", type=str, help="Workspace name")
     parser.add_argument("-p", "--engine", type=str, help="Simulation engine name")
